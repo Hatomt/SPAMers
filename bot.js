161ca3b44c28865154e45c7b1bf87185a99198c8
Hatomt <46413092+Hatomt@users.noreply.github.com>
2019-01-06 09:24:25 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get(531363219202244650)
+setInterval(function() {
+channel.send(HaTEm spam here hatem spam spam credits bot
+);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
